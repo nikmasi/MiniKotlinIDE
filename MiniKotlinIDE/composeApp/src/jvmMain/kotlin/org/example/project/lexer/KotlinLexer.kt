07768e1c7268ffc72678e5c7e4b07d93e2976cd1
nikmasi <138830925+nikmasi@users.noreply.github.com>
2026-01-05 11:03:41 +0100

    Add support for block comments (/* */)

--- MiniKotlinIDE/composeApp/src/jvmMain/kotlin/org/example/project/lexer/KotlinLexer.kt
+++ MiniKotlinIDE/composeApp/src/jvmMain/kotlin/org/example/project/lexer/KotlinLexer.kt
@@ -1,10 +1,6 @@
 package org.example.project.lexer
 
-//https://kotlinlang.org/docs/keyword-reference.html#operators-and-special-symbols
-
-class KotlinLexer(
-    private val source: String
-) {
+class KotlinLexer(private val source: String) {
     private var index = 0
 
     fun tokenize(): List<Token> {
@@ -20,14 +16,34 @@ class KotlinLexer(
                     tokens += Token(TokenType.WHITESPACE, c.toString(), start, index)
                 }
 
-                c == '/' && peek('/') -> {
-                    while (index < source.length && source[index] != '\n') index++
-                    tokens += Token(TokenType.COMMENT, source.substring(start, index), start, index)
+                c == '/' -> {
+                    when {
+                        peek('/') -> {
+                            while (index < source.length && source[index] != '\n') {
+                                index++
+                            }
+                            tokens += Token(TokenType.COMMENT, source.substring(start, index), start, index)
+                        }
+                        peek('*') -> {
+                            index += 2
+                            while (index < source.length) {
+                                if (source[index] == '*' && peek('/')) {
+                                    index += 2
+                                    break
+                                }
+                                index++
+                            }
+                            tokens += Token(TokenType.COMMENT, source.substring(start, index.coerceAtMost(source.length)), start, index)
+                        }
+                        else -> {
+                            index++
+                            tokens += Token(TokenType.OPERATOR, c.toString(), start, index)
+                        }
+                    }
                 }
 
                 c == '"' -> {
                     index++
-
                     while (index < source.length && source[index] != '"') {
                         if (source[index] == '\\' && index + 1 < source.length) {
                             index += 2
@@ -35,28 +51,24 @@ class KotlinLexer(
                             index++
                         }
                     }
-
-                    if (index < source.length) {
-                        index++
-                    }
-
-                    tokens += Token(
-                        TokenType.STRING,
-                        source.substring(start, index),
-                        start,
-                        index
-                    )
+                    if (index < source.length) index++
+                    tokens += Token(TokenType.STRING, source.substring(start, index), start, index)
                 }
 
-
                 c.isLetter() || c == '_' -> {
-                    while (index < source.length &&
-                        (source[index].isLetterOrDigit() || source[index] == '_')
-                    ) index++
-
+                    while (index < source.length && (source[index].isLetterOrDigit() || source[index] == '_')) {
+                        index++
+                    }
                     val word = source.substring(start, index)
                     val type = KotlinLanguage.resolve(word)
                     tokens += Token(type, word, start, index)
+                }
+
+                c.isDigit() -> {
+                    while (index < source.length && source[index].isDigit()) {
+                        index++
+                    }
+                    tokens += Token(TokenType.NUMBER, source.substring(start, index), start, index)
                 }
 
                 else -> {
@@ -65,9 +77,9 @@ class KotlinLexer(
                 }
             }
         }
-
         return tokens
     }
+
     private fun peek(expected: Char): Boolean =
         index + 1 < source.length && source[index + 1] == expected
 }
